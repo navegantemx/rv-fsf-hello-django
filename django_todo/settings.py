7e7f17c9d6b3ecac8d271e67104f7af76ef96d9f
navegantemx <raulvelazquez437@gmail.com>
2020-08-04 02:51:45 +0000

    Setup development enviroment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'i2yr#(1cfgr*%p8@1*5ot9-+k_4f7d+ab@mms5x8#cy3i(%uv7')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
